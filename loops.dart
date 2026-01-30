73d578a3585f6f317319a57b62e68e7c5b071f6b
Anit Shrestha <anitshrestha101@gmail.com>
2026-01-30 17:03:43 +0545

    initial commit

---
+++ loops.dart
@@ -0,0 +1,31 @@
+/*void main() {
+  //for loop
+  int i;
+  for (i = 1; i <= 5; i++) {
+    print("Hello, $i");
+  }
+}*/
+
+/*1. Sum of Numbers
+Write a program to calculate the sum of numbers from 1 to 50.
+*/
+
+// void main() {
+//   var sum = 0;
+//   for (int i = 1; i <= 50; i++) {
+//     sum = sum + i;
+//   }
+//   print("Total sum of 1 to 50 is $sum");
+// }
+
+//multiplication table of 2
+/*
+void main() {
+  int num = 2;
+
+  for (int i = 1; i <= 10; i++) {
+    int result = num * i;
+    print("$num * $i = $result");
+  }
+}
+*/
---
+++ variable.dart
@@ -0,0 +1,21 @@
+/*void main() {
+  int age = 22; //stores a number
+  String name = "Anit"; //stores text
+  bool isStudent = true; //stores true/false
+
+  print(name);
+  print(age);
+  print(isStudent);
+}*/
+
+// void main() {
+  // var name = "anit";
+  // name = "shrestha";
+  // print(name);
+
+  // dynamic name = "mamata";
+  // name = 20;
+  // print(name);
+// }
+
+
