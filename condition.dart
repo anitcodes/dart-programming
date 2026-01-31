c2ffd3e8782217c2999fdc763dbc76cc02e722e8
Anit Shrestha <anitshrestha101@gmail.com>
2026-01-31 19:55:49 +0545

    solving questions of condition and loops

---
+++ condition.dart
@@ -0,0 +1,88 @@
+//Questions
+//Beginner Level(Basics)
+
+//Conditions
+
+//1.Check whether a number is positive, negative, or zero.
+/*
+void main() {
+  int num = 0;
+  if (num > 0) {
+    print("$num is positive number.");
+  } else if (num < 0) {
+    print("$num is negative number");
+  } else {
+    print("$num is 0");
+  }
+}
+*/
+
+//2.Check whether a number is even or odd.
+/*
+void main() {
+  int num = 5;
+  if (num % 2 == 0) {
+    print("$num is even");
+  } else {
+    print("$num is odd");
+  }
+}
+*/
+
+//3.Check whether a person is eligible to vote (age ≥ 18).
+/*
+void main() {
+  int age = 15;
+  if (age >= 18) {
+    print("Congratulation! You can vote");
+  } else {
+    print("Sorry, Underage cannot vote");
+  }
+}
+*/
+
+//4.Find the largest of two numbers.
+/*
+void main() {
+  int num1 = 93;
+  int num2 = 24;
+  if (num1 > num2) {
+    print("$num1 is greater than $num2");
+  } else {
+    print("$num2 is greater than $num1");
+  }
+}
+*/
+
+//5. Check whether a character is a vowel or consonant.
+/*
+void main() {
+  String ch = "l";
+  String letter = ch.toLowerCase();
+
+  if (letter == 'a' ||
+      letter == 'e' ||
+      letter == 'i' ||
+      letter == 'o' ||
+      letter == 'u') {
+    print("$letter is vowel letter");
+  } else {
+    print("$letter is consonent");
+  }
+}
+*/
+
+//6. Check whether a year is a leap year.
+
+//7. Convert marks to grade using if–else.
+
+
+//Loops
+
+// 1.Print numbers from 1 to 10.
+// 2.Print numbers from 10 to 1.  
+// 3.Print all even numbers between 1 and 50.
+// 4.Print the multiplication table of a number.
+// 5.Find the sum of first n natural numbers.
+// 6.Count the number of digits in a number.  
+// 7.Print each character of a string.
--- loops.dart
+++ loops.dart
@@ -1,93 +1,21 @@
-/*void main() {
-  //for loop
-  int i;
-  for (i = 1; i <= 5; i++) {
-    print("Hello, $i");
+//substring
+
+/*
+void main(){
+  for(int i=1; i<=5; i++){
+    String greet = "hello world";
+    print(greet.substring(0, 5));
   }
 }*/
 
-/*1. Sum of Numbers
-Write a program to calculate the sum of numbers from 1 to 50.
-*/
+//Questions
 
-// void main() {
-//   var sum = 0;
-//   for (int i = 1; i <= 50; i++) {
-//     sum = sum + i;
-//   }
-//   print("Total sum of 1 to 50 is $sum");
-// }
+void main(){
 
-//multiplication table of 2
-/*
-void main() {
-  int num = 2;
+//1.
 
-  for (int i = 1; i <= 10; i++) {
-    int result = num * i;
-    print("$num * $i = $result");
-  }
-}
-*/
 
-// Write a program to calculate the sum of numbers from 1 to 100 using a for loop.
-/*void main() {
-  var sum = 0;
-  for (int i = 1; i <= 100; i++) {
-    sum += i;
-  }
-  print("Sum = $sum");
-}*/
 
-// Write a program to print all even numbers from 1 to 50 using a for loop.
-/*void main() {
-  for(int i=1; i<=50; i++){
-    if(i%2 == 0){
-      print(i);
-    }
-  }
-
-}*/
-
-//Write a program to calculate the factorial of a given number using a for loop.
-
-/*
-import 'dart:io';
-
-void main() {
-  print("Enter a number: ");
-  int number = int.parse(stdin.readLineSync()!);
-
-  int factorial = 1;
-
-  for (int i = 1; i <= number; i++) {
-    factorial *= i;
-  }
-
-  print("Factorial of $number is $factorial");
 }
 
-*/
-
-
-//factorial of 3
-// void main() {
-//   int number = 3;
-//   int factorial = 1;
-
-//   for (int i = 1; i <= number; i++) {
-//     factorial = factorial * i;
-//   }
-//   print(factorial);
-// }
-
-//for.. in loop example
-/*
-void main() {
-  List friendName = ["sudip", "bhim", "pratik"];
-  for (String friends in friendName) {
-    print(friends);
-  }
-}
-*/
 
